d48d5f00ea0be56dff6f1a6252f533089ce8187f
Miguel Alfaro <miguelalfaro2017@gmail.com>
2025-04-23 21:54:55 -0500

    refactor: any changes

--- app/settings.py
+++ app/settings.py
@@ -6,8 +6,7 @@ from mangum import Mangum
 # Configuración de MongoDB
 MONGODB_URI = os.getenv("MONGODB_URI")
 client = AsyncIOMotorClient(MONGODB_URI)
-MONGODB_NAME = os.getenv("MONGODB_NAME")
-db = client[MONGODB_NAME]
+db = client["welfare"]
 users_collection = db["users"]
 
 # Configuración JWT
