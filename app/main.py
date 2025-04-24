31a0a42f517500e72ec6edb72f94c1f9bb7d9aa3
Miguel Alfaro <miguelalfaro2017@gmail.com>
2025-04-23 21:48:41 -0500

    refactor: any changes

--- app/main.py
+++ app/main.py
@@ -50,7 +50,7 @@ async def login_for_access_token(form_data: UserCreate):
 
 @app.post(f"{MAIN_ROUTE}/diagnosis")
 async def test(current_user: User = Depends(get_current_user)):
-    return {"mensaje": "¡Hola Mundo Protegido gaaa!"}
+    return {"mensaje": "¡Hola Mundo Protegido gaaaaa!"}
 
 handler = Mangum(app)
 
